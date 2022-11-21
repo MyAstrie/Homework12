8a809a01052175f8eb2431ce26596bd3be082617
Andrey Zinchenko <astrokoit@gmail.com>
2022-11-21 22:40:44 +0700

    Сделано ДЗ

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,50 @@
-public class Book {
+import java.util.Arrays;
+
+public class Book extends Author {
+
+    //region Variables
+    private String bookName;
+    private Integer yearPublication;
+    //endregion
+
+    //region Constructors
+    Book(String bookName, String authorFullName) {
+        super(authorFullName);
+        try {
+            this.bookName = bookName;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    Book(String bookName, String authorFullName, Integer yearPublication) {
+        super(authorFullName);
+        try {
+            this.bookName = bookName;
+            this.yearPublication = yearPublication;
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    //endregion
+
+    //region print
+    public void printInfo(){
+        System.out.println("------Информация------");
+        System.out.println("Название книги: " + getBookName() +
+                "\nАвтор книги: " + getAuthorFullName() +
+                "\nГод книги: " + getYearPublication());
+        System.out.println("----------------------");
+    }
+    //endregion
+
+    //region bookName
+    public void setBookName(String bookName) {this.bookName = bookName;}
+    public String getBookName() {return this.bookName;}
+    //endregion
+
+    //region yearPublication
+    public void setYearPublication(int yearPublication) {this.yearPublication = yearPublication;}
+    public Integer getYearPublication() {return this.yearPublication;}
+    //endregion
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author firstAuthor = new Author("Максим Горький");
+        Author secondAuthor = new Author();
+        secondAuthor.setAuthorFullName("Виктор Пелевин");
+
+        Book firstBook = new Book("На дне", firstAuthor.getAuthorFullName(), 1902);
+        firstBook.printInfo();
+
+        Book secondBook = new Book("Generation «П»", secondAuthor.getAuthorFullName());
+        secondBook.printInfo();
+        secondBook.setYearPublication(1999);
+        secondBook.printInfo();
     }
 }
